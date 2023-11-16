5f7d4c293716a3a112b27b9aee14da48ef565601
patricijaboroveca <patricija.boroveca@grobinasskola.lv>
2023-11-16 15:19:41 +0200

    Init commit

---
+++ 5.uzdevums.py
@@ -0,0 +1,16 @@
+import time
+
+sobrid=time
+
+laiks= sobrid()
+
+if laiks<12:
+    print('labrīt!')
+
+elif laiks<19:
+    print('labdien!')
+
+else: 
+    print('labvakar!')
+
+    #nepabeigts, jo nesanaca
