78b47f4c225e37e8fb98d78158be5c8c8dc9aaa5
patricijaboroveca <patricija.boroveca@grobinasskola.lv>
2023-11-16 14:57:38 +0200

    Init commit

---
+++ 3.uzdevums.py
@@ -0,0 +1,8 @@
+#para vai nepara izmantojot if prieksrakstu
+skaitlis= int(input('Ievadiet skaitli:')) #paprasiju ievadit random ciparu
+
+if skaitlis %2!=0: #ja skaitlis dalās ar 2 un iznākumam ir atlikums, teoretiski vienkarsi iznakums nav vesels skaitlis
+    print('Ievadītais skaitlis ir nepāra!')
+
+else:
+    print('Ievadītais skaitlis ir pāra!')
